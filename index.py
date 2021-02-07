74fe882ce418f637c471172ad7353a462344aedd
Anxml <70949137+Anxml@users.noreply.github.com>
2021-02-07 19:20:32 +0530

    Fixed Bugs

--- index.py
+++ index.py
@@ -1,16 +1,21 @@
 from flask import *
 from sitedb import *
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField,TextAreaField,DecimalField,FileField
-from wtforms.validators import ValidationError,DataRequired,Length
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, DecimalField, FileField
+from wtforms.validators import ValidationError, DataRequired, Length
 from PIL import Image
 from datetime import datetime
-import random,string,os,socket,locale,time
+import random
+import string
+import os
+import socket
+import locale
+import time
 
 upload_folder = '/static/listthumb'
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__, static_folder='static')
 app.config['UPLOADED_PHOTOS_DEST'] = upload_folder
-app.secret_key = 'pqcwwmv'        
+app.secret_key = 'pqcwwmv'
 #
 #
 #   S P A C E
@@ -21,84 +26,112 @@ list_mgr.import_list(list_mgr)
 list_mgr.import_list_users(list_mgr)
 list_mgr.thumb(list_mgr)
 chats.importchatuser()
-locale.setlocale(locale.LC_ALL,'English_India')
+locale.setlocale(locale.LC_ALL, 'English_India')
 #
-#Forms
+# Forms
 #
+
+
 class SignupForm(FlaskForm):
-    username = StringField(label='Enter your permanent username',validators=[DataRequired(),Length(min=3,max=15)])
-    dispname = StringField(label='Enter your display name',validators=[DataRequired(),Length(min=3,max=30)])
-    paswd = PasswordField(label='Password',validators=[DataRequired(),Length(min=4,max=20)])
+    username = StringField(label='Enter your permanent username', validators=[
+                           DataRequired(), Length(min=3, max=15)])
+    dispname = StringField(label='Enter your display name', validators=[
+                           DataRequired(), Length(min=3, max=30)])
+    paswd = PasswordField(label='Password', validators=[
+                          DataRequired(), Length(min=4, max=20)])
     submit = SubmitField(label='Submit')
+
     def validate_username(self, username):
         excluded_chars = "*?!'^+%&/()=}][{$#Ⅱ Ⅰ,:"
         for char in self.username.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in username.")
+                raise ValidationError(
+                    f"Character {char} is not allowed in username.")
             if self.username.data in users.users_details:
                 raise ValidationError(f"Username Already Taken")
+
     def validate_dispname(self, dispname):
         excluded_chars = "*?!'^+%&/()=}][{$#ⅡⅠ,"
         for char in self.dispname.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in display name.")
+                raise ValidationError(
+                    f"Character {char} is not allowed in display name.")
+
     def validate_paswd(self, paswd):
         excluded_chars = "*Ⅱ Ⅰ"
         for char in self.paswd.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in password.")
+                raise ValidationError(
+                    f"Character {char} is not allowed in password.")
+
+
 class NewListingForm(FlaskForm):
-    lname = StringField(validators=[DataRequired(),Length(min=5,max=40)])
-    lprice = DecimalField(places=2,validators=[DataRequired(),Length(min=1)])
+    lname = StringField(validators=[DataRequired(), Length(min=5, max=40)])
+    lprice = DecimalField(places=2, validators=[DataRequired()])
     lfeature = TextAreaField(validators=[Length(min=5)])
-    lspecs = TextAreaField(validators=[DataRequired(),Length(min=5)])
-    laddr = TextAreaField(validators=[DataRequired(),Length(min=5)])
+    lspecs = TextAreaField(validators=[DataRequired(), Length(min=5)])
+    laddr = TextAreaField(validators=[DataRequired(), Length(min=5)])
     limg = FileField(validators=[DataRequired()])
     lsubmit = SubmitField(label='Submit')
-    def validate_lname(self,lname):
+
+    def validate_lname(self, lname):
         excluded_chars = "*?!'^+%&/()=}][{$#ⅡⅠ,"
         for char in self.lname.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in listing name.")
-    def validate_lfeature(self,lfeature):
+                raise ValidationError(
+                    f"Character {char} is not allowed in listing name.")
+
+    def validate_lfeature(self, lfeature):
         excluded_chars = "*?!'^+%&/()=}][{$#ⅡⅠ,"
         for char in self.lfeature.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in features.")
-    def validate_lspecs(self,lspecs):
+                raise ValidationError(
+                    f"Character {char} is not allowed in features.")
+
+    def validate_lspecs(self, lspecs):
         excluded_chars = "*?!'^+%&/()=}][{$#ⅡⅠ,"
         for char in self.lspecs.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in specifications.")
-    def validate_laddr(self,laddr):
+                raise ValidationError(
+                    f"Character {char} is not allowed in specifications.")
+
+    def validate_laddr(self, laddr):
         excluded_chars = "*?!'^+%&/()=}][{$#ⅡⅠ,"
         for char in self.laddr.data:
             if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed in username.")
+                raise ValidationError(
+                    f"Character {char} is not allowed in username.")
+
+
 #
 #Index (Home)
 #
-@app.route("/") 
+
+
+@app.route("/")
 def home():
     random.shuffle(list_mgr.thumbs)
     thumb = list_mgr.thumbs
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
-    return render_template('index.html',thumb=thumb,token=lgusertoken,dispname=dispname,lname=list_mgr.lists_data)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
+    return render_template('index.html', thumb=thumb, token=lgusertoken, dispname=dispname, lname=list_mgr.lists_data)
 #
-#Log In Page
+# Log In Page
 #
-@app.route('/login/', methods = ["POST","GET"])
+
+
+@app.route('/login/', methods=["POST", "GET"])
 def login():
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     err = str()
     if request.cookies.get('id') in sesh_manager.user_login:
         return redirect(url_for('lg_usr'))
     if request.method == 'POST':
         name = str(request.form['name'])
         paswd = str(request.form['pass'])
-        keeplogin = request.form['keeplogin'] #gets a T/F value of keeping the user logged in
+        # gets a T/F value of keeping the user logged in
+        keeplogin = request.form['keeplogin']
         if name == '':
             return redirect(url_for('login'))
         if name in users.users_details and paswd == users.users_details[name][1]:
@@ -109,44 +142,49 @@ def login():
                     randID = users.give_token(users)
                 except randID not in sesh_manager.user_login:
                     break
-            #give user a random ID
+            # give user a random ID
             sesh_manager.user_login[randID] = name
-            #save it to db of all users currently logged in
-            secure_lg_cookie = make_response(render_template('lg_success.html'))
-            #this is required to make a cookie
+            # save it to db of all users currently logged in
+            secure_lg_cookie = make_response(
+                render_template('lg_success.html'))
+            # this is required to make a cookie
             if keeplogin == 'True':
-                secure_lg_cookie.set_cookie('id',randID,max_age=60*60*24*365)
+                secure_lg_cookie.set_cookie('id', randID, max_age=60*60*24*365)
             else:
-                secure_lg_cookie.set_cookie('id',randID)
-            del randID,name,keeplogin
+                secure_lg_cookie.set_cookie('id', randID)
+            del randID, name, keeplogin
             return secure_lg_cookie
         else:
             redirect(url_for('login'))
             err = 'Incorrect UserID/Password'
-    return render_template('login.html',token=lgusertoken,dispname=dispname,err=err)
+    return render_template('login.html', token=lgusertoken, dispname=dispname, err=err)
 #
-#Signup
+# Signup
 #
-@app.route('/signup/', methods = ["POST","GET"])
+
+
+@app.route('/signup/', methods=["POST", "GET"])
 def signup():
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     form = SignupForm()
     if form.validate_on_submit():
         name = form.username.data
         dispname = form.dispname.data
         paswd = form.paswd.data
-        users.users_details[name]=[dispname,paswd]
-        users.save_usrdata(users,name,dispname,paswd)
+        users.users_details[name] = [dispname, paswd]
+        users.save_usrdata(users, name, dispname, paswd)
         return redirect(url_for('home'))
-    return render_template('signup.html',token=lgusertoken,dispname=dispname,form=form)
+    return render_template('signup.html', token=lgusertoken, dispname=dispname, form=form)
 #
-#Listing
+# Listing
 #
+
+
 @app.route('/list/<lno>')
 def listing(lno):
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     lname = list_mgr.lists_data[lno][0]
     lprice = list_mgr.lists_data[lno][1]
     lfeature = list_mgr.lists_data[lno][2]
@@ -159,24 +197,28 @@ def listing(lno):
         username = sesh_manager.user_login[usercookie]
     else:
         username = 'null'
-    return render_template('listing.html',token=lgusertoken,dispname=dispname,lno=lno,lname=lname,lauthu=lauthu,lauth=lauth,laddr=laddr,lspecs=lspecs,lfeature=lfeature,lprice=lprice,username=username)
+    return render_template('listing.html', token=lgusertoken, dispname=dispname, lno=lno, lname=lname, lauthu=lauthu, lauth=lauth, laddr=laddr, lspecs=lspecs, lfeature=lfeature, lprice=lprice, username=username)
 #
-#Image Page
+# Image Page
 #
+
+
 @app.route('/i/<img>')
 def img(img):
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     imgsrc = 'listthumb/' + img
     imgna = img.rstrip('.jpg')
-    return render_template('img.html',imgn = img,imgsrc = imgsrc,imgna = imgna,token=lgusertoken,dispname=dispname)
+    return render_template('img.html', imgn=img, imgsrc=imgsrc, imgna=imgna, token=lgusertoken, dispname=dispname)
 #
-#Page for logged in users
+# Page for logged in users
 #
-@app.route('/u/',methods=['GET', 'POST'])
+
+
+@app.route('/u/', methods=['GET', 'POST'])
 def lg_usr():
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     usercookie = request.cookies.get('id')
     if usercookie in sesh_manager.user_login:
         username = sesh_manager.user_login[usercookie]
@@ -184,53 +226,63 @@ def lg_usr():
             request.form['logout']
             sesh_manager.user_login.pop(usercookie)
             return redirect(url_for('home'))
-        return render_template('user.html',dispname=dispname,token=lgusertoken,username=username,thumb=list_mgr.user_lists[username],lname=list_mgr.lists_data)
-    else :
+        return render_template('user.html', dispname=dispname, token=lgusertoken, username=username, thumb=list_mgr.user_lists[username], lname=list_mgr.lists_data)
+    else:
         return redirect(url_for('login'))
 #
-#Create a listing
+# Create a listing
 #
-@app.route('/newlisting',methods=['GET','POST'])
+
+
+@app.route('/newlisting', methods=['GET', 'POST'])
 def newlisting():
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     if request.cookies.get('id') in sesh_manager.user_login:
         lform = NewListingForm()
         if lform.validate_on_submit():
             lno = list_mgr.create_lno(list_mgr)
             limg = lform.limg.data
-            lprice = locale.currency(lform.lprice.data,symbol=False,grouping=True)
-            limg.save('static/listthumb/%s.jpg'%lno)
+            lprice = locale.currency(
+                lform.lprice.data, symbol=False, grouping=True)
+            limg.save('static/listthumb/%s.jpg' % lno)
             lauth = sesh_manager.user_login[request.cookies.get('id')]
-            list_mgr.lists_data[lno] = [lform.lname.data,lprice,lform.lfeature.data,lform.lspecs.data,lform.laddr.data,lno,lauth]
-            list_mgr.save_list(list_mgr,lform.lname.data,lprice,lform.lfeature.data,lform.lspecs.data,lform.laddr.data,lno,lauth)
+            list_mgr.lists_data[lno] = [lform.lname.data, lprice,
+                                        lform.lfeature.data, lform.lspecs.data, lform.laddr.data, lno, lauth]
+            list_mgr.save_list(list_mgr, lform.lname.data, lprice, lform.lfeature.data,
+                               lform.lspecs.data, lform.laddr.data, lno, lauth)
             list_mgr.thumbs.append(lno)
-            print(list_mgr.lists_data[lno])
+            list_mgr.user_lists[lauth].append(lno)
+            print(list_mgr.lists_data[lno], list_mgr.user_lists[lauth])
             return redirect(url_for('lg_usr'))
-        return render_template('newlisting.html',dispname=dispname,token=lgusertoken,lform=lform)
+        return render_template('newlisting.html', dispname=dispname, token=lgusertoken, lform=lform)
     else:
         return redirect(url_for('home'))
 #
-#Chats
+# Chats
 #
+
+
 @app.route('/chats')
 def chatspg():
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     usercookie = request.cookies.get('id')
     if usercookie in sesh_manager.user_login:
         userid = sesh_manager.user_login[usercookie]
         contactsperuser = chats.chats_per_user[userid]
-        return render_template('chats.html',dispname=dispname,token=lgusertoken,allcontacts=contactsperuser)
+        return render_template('chats.html', dispname=dispname, token=lgusertoken, allcontacts=contactsperuser)
     else:
         return redirect(url_for('lg_usr'))
 #
-#Chats of each ID
+# Chats of each ID
 #
-@app.route('/chats/<chatno>',methods=['GET','POST'])
+
+
+@app.route('/chats/<chatno>', methods=['GET', 'POST'])
 def chatsid(chatno):
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     usercookie = request.cookies.get('id')
     if usercookie in sesh_manager.user_login:
         userid = sesh_manager.user_login[usercookie]
@@ -238,44 +290,53 @@ def chatsid(chatno):
         chatdata = chats.chats_list[chatno][1]
         if request.method == 'POST':
             sentmsg = request.form['msgbox']
-            chats.chats_list[chatno][1].append((userid,sentmsg))
-        return render_template('chatspu.html',dispname=dispname,token=lgusertoken,allcontacts=contactsperuser,chatdata=chatdata)
+            chats.chats_list[chatno][1].append((userid, sentmsg))
+        return render_template('chatspu.html', dispname=dispname, token=lgusertoken, allcontacts=contactsperuser, chatdata=chatdata)
     else:
         return redirect(url_for('lg_usr'))
 #
-#Start a Chat
+# Start a Chat
 #
+
+
 @app.route('/newchat/<lno>')
 def newchat(lno):
-    dispname = users.chk_usr_lg(users,1)
-    lgusertoken = users.chk_usr_lg(users,0)
+    dispname = users.chk_usr_lg(users, 1)
+    lgusertoken = users.chk_usr_lg(users, 0)
     usercookie = request.cookies.get('id')
     if usercookie in sesh_manager.user_login:
         chatreciever = list_mgr.lists_data[lno][6]
         chatsender = sesh_manager.user_login[usercookie]
-        if chatsender in chats.chats_per_user[chatreciever][0]:
+        chatsender_contacts = list()
+        for each_chat_metadata in chats.chats_per_user[chatsender]:
+            chatsender_contacts.append(each_chat_metadata[0])
+        if chatreciever in chatsender_contacts:
             return redirect('/chats')
         if chatsender == chatreciever:
             return redirect(url_for('lg_usr'))
-        chatno = chats.newchatstartfunc(chatsender,chatreciever,lno) 
-        return redirect('/chats/%s'%chatno)
+        chatno = chats.newchatstartfunc(chatsender, chatreciever, lno)
+        return redirect('/chats/%s' % chatno)
     else:
-        return redirect('/list/%s'%lno)
+        return redirect('/list/%s' % lno)
 #
-#Error Handling
+# Error Handling
 #
+
+
 @app.errorhandler(500)
 def pnf500(e):
-        return render_template('404.html'), 500
+    return render_template('404.html'), 500
+
 
 @app.errorhandler(404)
 def pnf(e):
-        return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 #
-#Execution
+# Execution
 #
 if __name__ == "__main__":
     PCNAME = socket.gethostname()
-    #this gets your device name which gets your local ip addresss
-    app.run(host=socket.gethostbyname(PCNAME),debug=True)
+    # this gets your device name which gets your local ip addresss
+    app.run(host=socket.gethostbyname(PCNAME), debug=True)
--- sitedb.py
+++ sitedb.py
@@ -1,69 +1,84 @@
-import random,string,os
+import random
+import string
+import os
 from flask import *
 import time
-#contains all of the variable storage and stuff for index.py
-#user database with username, Display Name, password 
+# contains all of the variable storage and stuff for index.py
+# user database with username, Display Name, password
 #
-#Manages Sessions
+# Manages Sessions
 #
+
+
 class sesh_manager():
-    user_login={}
+    user_login = {}
 #
-#Manages User Login Data
+# Manages User Login Data
 #
+
+
 class users():
-    users_details={'asdf':['Debug User','qwer']}
-    #users_details={'asdf':'qwer'}
-    #imports userdata from userdata.txt
+    users_details = {'asdf': ['Debug User', 'qwer']}
+    # users_details={'asdf':'qwer'}
+    # imports userdata from userdata.txt
+
     def import_usrdata(self):
-        usrdata_file = open('sdb/userdata.json',encoding='utf-8')
+        usrdata_file = open('sdb/userdata.json', encoding='utf-8')
         oneusrdta = usrdata_file.read().split(':')
         for usrdta in oneusrdta:
             eachdatafield = usrdta.split(',')
-            users.users_details[eachdatafield[0]]=[eachdatafield[1],eachdatafield[2]]
+            users.users_details[eachdatafield[0]] = [
+                eachdatafield[1], eachdatafield[2]]
         usrdata_file.close()
-    #saves username data to userdata.txt
-    def save_usrdata(self,usrnm,dispname,pswd):
-        usrdata_file = open('sdb/userdata.json','a',encoding='utf-8')
-        usrdata_file.write(':%s,%s,%s'%(usrnm,dispname,pswd))
+    # saves username data to userdata.txt
+
+    def save_usrdata(self, usrnm, dispname, pswd):
+        usrdata_file = open('sdb/userdata.json', 'a', encoding='utf-8')
+        usrdata_file.write(':%s,%s,%s' % (usrnm, dispname, pswd))
         usrdata_file.close()
-    #gives token to user to use instead of password(default length 20)
-    def give_token(self,l=38):
+    # gives token to user to use instead of password(default length 20)
+
+    def give_token(self, l=38):
         chars = string.digits+string.ascii_lowercase+string.ascii_uppercase
         token = str()
         for i in range(l):
             token += random.choice(chars)
         return token
-    #checks if user is logged in using cookies
-    def chk_usr_lg(self,u):
+    # checks if user is logged in using cookies
+
+    def chk_usr_lg(self, u):
         if request.cookies.get('id') in sesh_manager.user_login.keys():
             lgusertoken = 1
-            #if lgusertoken is 1 it gives user page link
+            # if lgusertoken is 1 it gives user page link
             if u == 1:
                 userid = sesh_manager.user_login[request.cookies.get('id')]
                 username = users.users_details[userid][0]
-                #username is checked in total logged in user by using cookies,1 is used as it is the second in a list
+                # username is checked in total logged in user by using cookies,1 is used as it is the second in a list
                 return username
         else:
             lgusertoken = 0
-            #if lgusertoken is 0 gives a login button
+            # if lgusertoken is 0 gives a login button
         return lgusertoken
 #
-#Listing Manager
+# Listing Manager
 #
+
+
 class list_mgr():
     user_lists = {
-        'qwer':['dQw4w9WgXcQ']
+        'qwer': ['dQw4w9WgXcQ']
     }
     lists_data = {
-        'dQw4w9WgXcQ':['My Test Advertisement','6,980.00','Some Feature','Some Specs','43rd Street,\nMain Area,\nBruh City','dQw4w9WgXcQ','qwer']
+        'dQw4w9WgXcQ': ['My Test Advertisement', '6,980.00', 'Some Feature', 'Some Specs', '43rd Street,\nMain Area,\nBruh City', 'dQw4w9WgXcQ', 'qwer']
     }
     thumbs = list()
+
     def thumb(self):
         for x in os.listdir(path='static/listthumb'):
             list_mgr.thumbs.append(x.rstrip('.jpg'))
-            #list_mgr.thumbs.append((x.rstrip('.jpg'),list_mgr.lists_data[x.rstrip('.jpg')][0]))
-    def create_lno(self,lnol=12):
+            # list_mgr.thumbs.append((x.rstrip('.jpg'),list_mgr.lists_data[x.rstrip('.jpg')][0]))
+
+    def create_lno(self, lnol=12):
         chars = string.digits
         genlno = str()
         for i in range(lnol):
@@ -71,33 +86,42 @@ class list_mgr():
         if genlno in list_mgr.lists_data:
             list_mgr.create_lno(list_mgr)
         return genlno
+
     def import_list_users(self):
         for usersl in users.users_details.keys():
-            list_mgr.user_lists[usersl]=[]
+            list_mgr.user_lists[usersl] = []
         for ldata in list_mgr.lists_data:
-            list_mgr.user_lists[list_mgr.lists_data[ldata][6]].append(list_mgr.lists_data[ldata][5])
-    def save_list(self,lname,lprice,lfeature,lspecs,laddr,lno,lauth):
-        usrdata_file = open('sdb/ldata.json','a',encoding='utf-16-le')
-        usrdata_file.write('Ⅱ%sⅠ%sⅠ%sⅠ%sⅠ%sⅠ%sⅠ%s'%(lname,lprice,lfeature,lspecs,laddr,lno,lauth))
+            list_mgr.user_lists[list_mgr.lists_data[ldata]
+                                [6]].append(list_mgr.lists_data[ldata][5])
+
+    def save_list(self, lname, lprice, lfeature, lspecs, laddr, lno, lauth):
+        usrdata_file = open('sdb/ldata.json', 'a', encoding='utf-16-le')
+        usrdata_file.write('Ⅱ%sⅠ%sⅠ%sⅠ%sⅠ%sⅠ%sⅠ%s' % (
+            lname, lprice, lfeature, lspecs, laddr, lno, lauth))
         usrdata_file.close
+
     def import_list(self):
-        list_data = open('sdb/ldata.json',encoding='utf-16-le')
+        list_data = open('sdb/ldata.json', encoding='utf-16-le')
         onelistdata = list_data.read().split('Ⅱ')
         for eachitem in onelistdata:
             eachdatafield = eachitem.split('Ⅰ')
-            list_mgr.lists_data[eachdatafield[5]]=[eachdatafield[0],eachdatafield[1],eachdatafield[2],eachdatafield[3],eachdatafield[4],eachdatafield[5],eachdatafield[6]]        
+            list_mgr.lists_data[eachdatafield[5]] = [eachdatafield[0], eachdatafield[1],
+                                                     eachdatafield[2], eachdatafield[3], eachdatafield[4], eachdatafield[5], eachdatafield[6]]
         list_data.close()
 #
-#Chat Functionality
+# Chat Functionality
 #
+
+
 class chats():
     chats_list = {
-        '11012':[('qwer','anmol'),[('qwer','anmol'),('qwer','hello guyy'),('anmol','Hii')]]
+        '11012': [('qwer', 'anmol'), [('qwer', 'anmol'), ('qwer', 'hello guyy'), ('anmol', 'Hii')]]
     }
     chats_per_user = {
-        'qwer':[('anmol','11012')],
-        'anmol':[('qwer','11012')]
+        'qwer': [('anmol', '11012')],
+        'anmol': [('qwer', '11012')]
     }
+
     def newchatno(cno=6):
         chars = string.digits
         chatno = str()
@@ -106,23 +130,27 @@ class chats():
         if chatno in chats.chats_list:
             chats.newchatno()
         return chatno
+
     def importchatuser():
         for eachuser in users.users_details.keys():
             if eachuser in chats.chats_per_user:
                 pass
             else:
                 chats.chats_per_user[eachuser] = []
-    def newchatstartfunc(chatsender,chatreciever,lno):
+
+    def newchatstartfunc(chatsender, chatreciever, lno):
         contactlist = list_mgr.lists_data[lno][0]
         newchatno = chats.newchatno()
-        chats.chats_list[newchatno] = [(chatsender,chatreciever),[('System','%s contacted %s for %s'%(chatsender,chatreciever,contactlist))]]
+        chats.chats_list[newchatno] = [(chatsender, chatreciever), [(
+            'System', '%s contacted %s for %s' % (chatsender, chatreciever, contactlist))]]
+
         if chatsender in chats.chats_per_user:
-            chats.chats_per_user[chatsender].append((chatreciever,newchatno))
+            chats.chats_per_user[chatsender].append((chatreciever, newchatno))
         else:
-            chats.chats_per_user[chatsender] = [(chatreciever,newchatno)]
+            chats.chats_per_user[chatsender] = [(chatreciever, newchatno)]
 
         if chatreciever in chats.chats_per_user:
-            chats.chats_per_user[chatreciever].append((chatsender,newchatno))
+            chats.chats_per_user[chatreciever].append((chatsender, newchatno))
         else:
-            chats.chats_per_user[chatreciever] = [(chatsender,newchatno)]
+            chats.chats_per_user[chatreciever] = [(chatsender, newchatno)]
         return newchatno
